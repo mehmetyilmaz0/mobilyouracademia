dae4f479d36f42a28b95f79d0a8a97032f7e53bd
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2020-01-03 19:41:47 +0300

    initial commits

--- src/screens/Detail/index.js
+++ src/screens/Detail/index.js
@@ -3,29 +3,15 @@ import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
 
 export default class Detail extends Component {
 
-    static navigationOptions = ({navigation}) => {
-
-        const article = navigation.getParam('article');
-        return {
-            title: article.title,
-            content: article.content,
-            keywords: article.keywords,
-            source: article.source,
-            user: article.user,
-            date: article.date,
-            favCount: article.favCount,
-            displayCount: article.displayCount,
-            imageUrl: article.imageUrl,
-            categoryTitle: article.category.title,
-            categoryContent: article.category.title,
-            categoryDate: article.category.date,
-        }
-    };
+    constructor(props) {
+        super(props);
+        this.item = props.navigation.getParam('article');
+    }
 
     render() {
         return (
             <SafeAreaView>
-                <Text>Detail</Text>
+                <Text>{this.item.content}</Text>
             </SafeAreaView>
         );
     }
